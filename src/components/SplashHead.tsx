91b284265dbbaa2a5b77261b86913c55c0bc81d2
marcushurn <marcushurn@hotmail.com>
2022-12-15 14:57:34 -0800

    styled header

--- src/components/SplashHead.tsx
+++ src/components/SplashHead.tsx
@@ -2,9 +2,14 @@ import { FunctionComponent } from "react";
 import './splashHead.css';
 
 const SplashHead: FunctionComponent = () => {
-    return ( 
-        <h1 id="title">Rover Images</h1>
-     );
+    return (
+        <header>
+            <div id="title">Rover Images</div>
+            <div id="text">
+                Explore a comprehensive collection of photographs from NASA's Curiosity, Opportunity, and Spirit rovers.
+            </div>
+        </header>
+    );
 }
- 
+
 export default SplashHead;

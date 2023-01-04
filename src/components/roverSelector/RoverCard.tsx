04a1b148a7c1e49777add06707d722cf4011e311
marcushurn <marcushurn@hotmail.com>
2023-01-03 16:15:04 -0800

    rover selector cards now responsive

--- src/components/roverSelector/RoverCard.tsx
+++ src/components/roverSelector/RoverCard.tsx
@@ -15,28 +15,32 @@ const RoverCard: FunctionComponent<RoverCardProps> = ({ roverName }) => {
       <div className={styles.textbox}>
         <div className={styles.roverName}>{roverName}</div>
         <table>
-          <tr>
-            <td>Lauch Date</td>
-            <td>11-02-2021</td>
-          </tr>
-          <tr>
-            <td>Lauch Date</td>
-            <td>2012-01-26</td>
-          </tr>
-          <tr>
-            <td>End Date</td>
-            <td>2021-03-08</td>
-          </tr>
+          <tbody>
+            <tr>
+              <td>Lauch Date</td>
+              <td>11-02-2021</td>
+            </tr>
+            <tr>
+              <td>Lauch Date</td>
+              <td>2012-01-26</td>
+            </tr>
+            <tr>
+              <td>End Date</td>
+              <td>2021-03-08</td>
+            </tr>
+          </tbody>
         </table>
         <table>
-          <tr>
-            <td>Total sols</td>
-            <td>1123</td>
-          </tr>
-          <tr>
-            <td>Image count</td>
-            <td>65433</td>
-          </tr>
+          <tbody>
+            <tr>
+              <td>Total sols</td>
+              <td>1123</td>
+            </tr>
+            <tr>
+              <td>Image count</td>
+              <td>65433</td>
+            </tr>
+          </tbody>
         </table>
       </div>
       {roverName === rover?.roverName && <hr />}
--- src/components/roverSelector/index.tsx
+++ src/components/roverSelector/index.tsx
@@ -1,10 +1,11 @@
 import { FunctionComponent } from "react";
 import RoverCard from "./RoverCard";
+import styles from './index.module.css';
 
 const RoverSelector: FunctionComponent = () => {
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <div className={styles.infobox}>
       <RoverCard roverName='Spirit' />
       <RoverCard roverName='Opportunity' />
       <RoverCard roverName='Curiosity' />

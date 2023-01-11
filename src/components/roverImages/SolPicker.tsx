9bc88cffe30c3f0ccd8842dfc3238349664d2c3e
marcushurn <marcushurn@hotmail.com>
2023-01-11 14:49:35 -0800

    add sol input field

--- src/components/roverImages/SolPicker.tsx
+++ src/components/roverImages/SolPicker.tsx
@@ -1,9 +1,24 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import NextBackBtn from "./NextBackBtn";
 import styles from './solPicker.module.css';
 
 
 const SolPicker: FunctionComponent = () => {
+  const [solValue, setSolValue] = useState(1);
+
+  const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    const num = parseInt(evt.target.value);
+    //if solValue !== currentSol
+    //  hide next/back buttons
+    //  display GoTo button
+    //willl require a handler (prop) to set new page
+    setSolValue(num);
+  }
+
+  const goToSol = () => {
+    alert('go to ' + solValue.toString());
+  }
+
   const nextSol = () => {
     alert("next day");
   }
@@ -17,8 +32,8 @@ const SolPicker: FunctionComponent = () => {
       <NextBackBtn goFunction={prevSol}>
         &#10094;
       </NextBackBtn>
-      sol 3 of 22343
-      <NextBackBtn goFunction={nextSol}>
+      sol <input type="number" value={solValue} onChange={e => handleChange(e)} /> of 2341
+      <NextBackBtn goFunction={solValue === 3 ? goToSol : nextSol}>
         &#10095;
       </NextBackBtn>
     </div>

d72767f6e6cf255a21cc1a69edab3793efd6258c
marcushurn <marcushurn@hotmail.com>
2023-01-24 10:50:38 -0800

    test roverselector

--- cypress/e2e/roverselector.cy.ts
+++ cypress/e2e/roverselector.cy.ts
@@ -1,8 +1,18 @@
 /// <reference types="cypress" />
 // @ts-check
 
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+
+describe('roverselector spec' ,() => {
+  
+  it('correctly switched rovers', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('header').contains('Rover Images');
+
+    cy.contains('As a counterpart to the opportunity rover, Spirit broadened the search for evidence of water which could have supported life on Mars.');
+  
+    cy.contains('2003-07-07').click();
+
+    cy.contains('Opportunity set about covering over 28 miles or Martian surface in search of evidence that water may have existed on the planet.');
+  });
+
 })
